60df54bb837a5eb1b701e887574202100a958a26
zhengjinguang829 <12345678>
2017-04-26 15:23:53 +0800

    add image

--- app/src/main/java/com/zjg/enjoy/weexfinance/activity/MainActivity.java
+++ app/src/main/java/com/zjg/enjoy/weexfinance/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zjg.enjoy.weexfinance;
+package com.zjg.enjoy.weexfinance.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zjg.enjoy.weexfinance.R;
 
 public class MainActivity extends AppCompatActivity {
 
